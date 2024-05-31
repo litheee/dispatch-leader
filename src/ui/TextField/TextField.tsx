ee08f3579b532c4ad34b1f72bb4da11342ec0ace
strix547 <strix547@mail.ru>
2024-05-31 20:53:08 +0300

    fix text-field

--- src/ui/TextField/TextField.tsx
+++ src/ui/TextField/TextField.tsx
@@ -4,14 +4,14 @@ import { InputHTMLAttributes } from 'react'
 import classes from './TextField.module.css'
 
 type TextFieldProps = {
-  label: string
+  label?: string
 } & InputHTMLAttributes<HTMLInputElement>
 
 export const TextField = ({ className, label, ...props }: TextFieldProps) => {
   return (
     <div className={classes['text-field']}>
       <input placeholder={label} {...props} className={cn(classes.input, className)} />
-      <label className={classes.label}>{label}</label>
+      {label ? <label className={classes.label}>{label}</label> : null}
     </div>
   )
 }
